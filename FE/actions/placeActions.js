ad7f87198460e74f10aeb19e83c478445f8ceb07
semianchuk <fix20152@gmail.com>
2017-05-23 21:19:05 +0300

    fix text

--- FE/actions/placeActions.js
+++ FE/actions/placeActions.js
@@ -4,6 +4,26 @@
 import axios from "axios";
 import config from "../components/config"
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
 export function fetchPlace(dispatch) {
         var url =  config.domain + 'place/get',
             self=this;
@@ -14,6 +34,7 @@ export function fetchPlace(dispatch) {
             success: function (obj) {
                 if(obj.status == 'success'){
                     var temp;
+                    obj.places = shuffle(obj.places);
                     for(var i=0; i<obj.places.length; i++){
                         if(obj.places[i].id == 2451){
                             temp = obj.places[i];
